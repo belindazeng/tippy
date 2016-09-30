04e62a876d266d1f37f75a2fe96dfd0dcb2ce3f5
Belinda Zeng <belindazeng@college.harvard.edu>
2016-09-30 16:14:58 -0700

    minimum reqs with tab view

---
+++ tippy/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  tippy
+//
+//  Created by Eden on 9/30/16.
+//  Copyright © 2016 Eden. All rights reserved.
+//
+
+import Cocoa
+
+class SettingsViewController: UIViewController {
+
+}
